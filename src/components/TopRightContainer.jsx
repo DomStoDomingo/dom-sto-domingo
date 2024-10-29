1bd8e2ba3e62083aba5cf04774efda6619e701ca
Dom <andrea.abrenica@via-appia.ph>
2024-10-30 01:10:13 +0800

    103024110

--- src/components/TopRightContainer.jsx
+++ src/components/TopRightContainer.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "../styles/TopRightContainer.css";
 import Contact from "./Contact";
+import linkedinImage from "../assets/svg/linkedin.svg";
 
 const TopRightContainer = () => {
     return(
         <div className="tl-container flex flex-col gap-4 flex-auto">
-            <div className="contacts-container glass flex justify-center items-center gap-4">
-                <Contact image="../assets/svg/linkedin.svg" alt="linkedin"/>
+            <div className="contacts-container flex justify-center items-center flex-wrap position-relative gap-4">
+                <Contact image={linkedinImage} alt="linkedin"/>
+                <Contact image={linkedinImage} alt="linkedin"/>
+                <Contact image={linkedinImage} alt="linkedin"/>
+                <Contact image={linkedinImage} alt="linkedin"/>
             </div>
             <div className="contacts-container glass flex justify-center items-center p-2">
                 s
